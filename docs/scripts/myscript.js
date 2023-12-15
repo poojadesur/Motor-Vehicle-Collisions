fb188a85a210b39b632180ad8ae51cf7de65eb89
Pooja Desur <poojadesur@gmail.com>
2023-12-14 23:07:26 -0500

    added sig tests to interactivity

--- docs/scripts/myscript.js
+++ docs/scripts/myscript.js
@@ -2,79 +2,312 @@
 
 // "https://raw.githubusercontent.com/poojadesur/Motor-Vehicle-Collisions/main/collisions.csv"
 
-// https://github.com/poojadesur/Motor-Vehicle-Collisions/blob/main/collisions.csv
+function zTestForProportions(p1, p2, n1, n2) {
+  const pCombined = (p1 * n1 + p2 * n2) / (n1 + n2);
+  const standardError = Math.sqrt((pCombined * (1 - pCombined)) * ((1 / n1) + (1 / n2)));
+  const zScore = (p1 - p2) / standardError;
+  return zScore;
+}
 
-// https://github.com/poojadesur/Motor-Vehicle-Collisions/blob/main/subset_100_rows.csv
+let borough1 = "";
+let borough2 = "";
 
-// Define the dimensions for the SVG container and margins for the chart
-const margin = { top: 20, right: 20, bottom: 50, left: 50 };
-const width = 600 - margin.left - margin.right;
-const height = 400 - margin.top - margin.bottom;
+function computeSignificance(p1, p2, n1, n2)
+{
+  // Perform Z-test for proportions
+  const zScore = zTestForProportions(p1, p2, n1, n2);
 
-// Append an SVG element to the body of the HTML
-const svg = d3
-  .select('div#plot')
-  .append('svg')
-  .attr('width', width + margin.left + margin.right)
-  .attr('height', height + margin.top + margin.bottom)
-  .append('g')
-  .attr('transform', `translate(${margin.left},${margin.top})`);
+  // Calculate significance level
+  const alpha = 0.05; // Set a significance level (e.g., 0.05)
 
-// Load the CSV data using promises
-d3.csv('https://raw.githubusercontent.com/poojadesur/Motor-Vehicle-Collisions/main/subset_100_rows.csv')
-  .then(function (data) {
-    // Filter out rows with null values in 'collision_id', 'number_of_persons_killed', and 'borough' columns
+  // Determine if the difference is statistically significant
+  if (Math.abs(zScore) > 1.96) { // For a two-tailed test and alpha = 0.05
+    isSig = "True";
+    console.log("There is a statistically significant difference between the populations.");
+  } else {
+    isSig = "False";
+    console.log("There is no statistically significant difference between the populations.");
+  }
 
-    console.log("Total rows in data:", data.length);
+  printTextOnPage(isSig);
 
-    // Group data by 'borough' and calculate total number of persons killed in each borough
-    const groupedData = d3.group(data, d => d.BOROUGH);
-    const aggregatedData = Array.from(groupedData, ([key, value]) => ({
-      BOROUGH: key,
-      totalKilled: d3.sum(value, d => +d.number_of_persons_killed), // Calculate sum of persons killed
-    }));
+}
 
-    // Define x and y scales
-    const x = d3.scaleBand()
-      .domain(aggregatedData.map(d => d.BOROUGH))
-      .range([0, width])
-      .padding(0.1);
+let year1 = ''; // Default selected year
+let year2 = ''; // Default selected year
+let isSig = ''
 
-    const y = d3.scaleLinear()
-      .domain([0, d3.max(aggregatedData, d => d.totalKilled)])
-      .nice()
-      .range([height, 0]);
+function printTextOnPage(isSig) {
+  let displayText = '';
 
-    // Create x-axis
-    svg.append('g')
-      .attr('transform', `translate(0,${height})`)
-      .call(d3.axisBottom(x))
-      .selectAll('text')
-      .attr('transform', 'rotate(-45)')
-      .style('text-anchor', 'end');
+  if (isSig === 'True') {
+    displayText = "There is a statistically significant difference between the populations. This means one is much more dangerous than the other.";
+  } else if (isSig === 'False') {
+    displayText = "There is no statistically significant difference between the populations. This means the boroughs and the timeframes provided are not that different from each other.";
+  } else {
+    displayText = "Are these two sets statistically significantly different from the other?";
+  }
 
-    // Create y-axis
-    svg.append('g')
-      .call(d3.axisLeft(y));
+  // Update the content of the <p> element with id 'displayText'
+  document.getElementById('displayText').innerHTML = displayText;
+}
 
-    // Create bars for the bar chart
-    svg.selectAll('.bar')
-      .data(aggregatedData)
-      .enter()
-      .append('rect')
-      .attr('class', 'bar')
-      .attr('x', d => x(d.BOROUGH))
-      .attr('y', d => y(d.totalKilled))
-      .attr('width', x.bandwidth())
-      .attr('height', d => height - y(d.totalKilled))
-      .attr('fill', 'steelblue');
+// Call the function with a specific value
+printTextOnPage('something'); // Change the value here
+
+// Function to handle click on year radio buttons
+function handleClickSet1Year(value) {
+  year1 = value;
+  console.log("Selected Year 1:", year1);
+}
+
+// Function to handle click on year radio buttons
+function handleClickSet2Year(value) {
+  year2 = value;
+  console.log("Selected Year 2:", year2);
+}
+
+
+function handleClickSet1(borough) {
+      borough1 = borough;
+      console.log("Selected borough from Set 1: " + borough);
+    }
+
+function handleClickSet2(borough) {
+  borough2 = borough;
+  console.log("Selected borough from Set 2: " + borough);
+}
+
+function getSignificance() {
+  let key1 = `${borough1}_${year1}`;
+  let key2 = `${borough2}_${year2}`;
+  console.log(key1, key2)
+  console.log(aggregatedDatawKey)
+  let deadliness1 = aggregatedDatawKey[key1].killed / aggregatedDatawKey[key1].total
+  let deadliness2 = aggregatedDatawKey[key2].killed / aggregatedDatawKey[key2].total
+
+  computeSignificance(deadliness1, deadliness2, aggregatedDatawKey[key1].total, aggregatedDatawKey[key2].total)
+}
+
+// Array to store fetched data from CSVs
+let allData = [];
+const fileRowCount = [];
+let curFileNameIndex = 0;
+let curFilerow = 0;
+let aggregatedDatawKey = {}
+
+// List of file names to fetch
+const files = [
+  'BRONX_2021.csv',
+  'BROOKLYN_2021.csv',
+  'MANHATTAN_2021.csv',
+  'QUEENS_2021.csv',
+  'STATENISLAND_2021.csv',
+  'BRONX_2022.csv',
+  'BROOKLYN_2022.csv',
+  'MANHATTAN_2022.csv',
+  'QUEENS_2022.csv',
+  'STATENISLAND_2022.csv'
+];
+
+// Fetch data from each file
+const promises = files.map(file => {
+  return d3.csv(`https://raw.githubusercontent.com/poojadesur/Motor-Vehicle-Collisions/d3/d3data/${file}`)
+    .then(data => {
+      allData = allData.concat(data);
+      if(fileRowCount.length == 0)
+      {
+        fileRowCount.push(data.length)
+        return data;
+      }
+      fileRowCount.push(data.length + fileRowCount[fileRowCount.length - 1]); // Store the number of rows for this file
+      return data;
+    });
+});
+
+Promise.all(promises)
+  .then(dataArray => {
+    // Aggregate data from all files into a single array
+    allData = dataArray.flat();
+
+    console.log(fileRowCount)
+
+    // Process and aggregate data
+    const aggregatedData = processData(allData, fileRowCount);
+    console.log(aggregatedDatawKey['STATENISLAND_2021'].killed)
+
+    createBarChartForKilled(aggregatedData); // For killed count
+    createBarChartForInjured(aggregatedData); // For injured count
+
+
   })
-  .catch(function (error) {
-    // Handle any errors while loading the data
-    console.log(error);
+  .catch(error => {
+    // Handle error
+    console.error('Error fetching data:', error);
   });
 
+function processData(data) {
+
+  // Extracting borough and year from filenames
+  data.forEach((fileData, index) => {
+    if (index > fileRowCount[curFileNameIndex])
+    {
+      curFileNameIndex += 1;
+      console.log(curFileNameIndex)
+      console.log(index)
+    }
+    const fileName = files[curFileNameIndex]; // Assuming 'files' array holds the filenames
+    const [borough, year] = fileName.replace('.csv', '').split('_');
+
+    // Aggregate counts for each borough and year
+    const key = `${borough}_${year}`;
+    if (!aggregatedDatawKey[key]) {
+      console.log(key)
+      aggregatedDatawKey[key] = {
+        borough,
+        year,
+        killed: 0,
+        injured: 0,
+        total: 0,
+      };
+    }
+
+    aggregatedDatawKey[key].killed += +fileData['NUMBER.OF.PERSONS.KILLED'] || 0;
+    aggregatedDatawKey[key].injured += +fileData['NUMBER.OF.PERSONS.INJURED'] || 0;
+    aggregatedDatawKey[key].total += 1;
+  });
+
+  return Object.values(aggregatedDatawKey);
+}
 
 
+// Function to create a bar chart for number of people killed
+function createBarChartForKilled(data) {
+  const margin = { top: 30, right: 30, bottom: 60, left: 60 };
+  const width = 300 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
+
+  const svgKilled = d3.select("div#plot")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  const xScaleKilled = d3.scaleBand()
+    .domain(data.map(d => `${d.borough}_${d.year}`))
+    .range([0, width])
+    .padding(0.1);
+
+  const yScaleKilled = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.killed)])
+    .nice()
+    .range([height, 0]);
+
+  svgKilled.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", d => xScaleKilled(`${d.borough}_${d.year}`))
+    .attr("width", xScaleKilled.bandwidth())
+    .attr("y", d => yScaleKilled(d.killed))
+    .attr("height", d => height - yScaleKilled(d.killed))
+    .attr("fill", "red");
+
+  svgKilled.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScaleKilled))
+    .selectAll("text") // Selects all the text elements of x-axis
+    .attr("transform", "rotate(-45)") // Rotates the text by -45 degrees
+    .style("text-anchor", "end"); // Adjusts text anchor
+
+  svgKilled.append("text")
+    .attr("x", width / 2)
+    .attr("y", 0 - (margin.top / 2))
+    .attr("text-anchor", "middle")
+    .style("font-size", "18px")
+    .text("People Killed in 2021 and 2022");
+
+  svgKilled.append("text")
+    .attr("transform", `translate(${width / 2},${height + margin.top + 20})`)
+    .style("text-anchor", "middle")
+    .text("Boroughs");
+
+  svgKilled.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Number of People Killed");
+
+  svgKilled.append("g")
+    .call(d3.axisLeft(yScaleKilled));
+}
+
+// Function to create a bar chart for number of people injured
+function createBarChartForInjured(data) {
+  const margin = { top: 30, right: 30, bottom: 60, left: 60 };
+  const width = 300 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
+
+  const svgInjured = d3.select("div#plot")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  const xScaleInjured = d3.scaleBand()
+    .domain(data.map(d => `${d.borough}_${d.year}`))
+    .range([0, width])
+    .padding(0.1);
+
+  const yScaleInjured = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.injured)])
+    .nice()
+    .range([height, 0]);
+
+  svgInjured.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", d => xScaleInjured(`${d.borough}_${d.year}`))
+    .attr("width", xScaleInjured.bandwidth())
+    .attr("y", d => yScaleInjured(d.injured))
+    .attr("height", d => height - yScaleInjured(d.injured))
+    .attr("fill", "blue");
+
+  svgInjured.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScaleInjured))
+    .selectAll("text") // Selects all the text elements of x-axis
+    .attr("transform", "rotate(-45)") // Rotates the text by -45 degrees
+    .style("text-anchor", "end"); // Adjusts text anchor
+
+  svgInjured.append("text")
+    .attr("transform", `translate(${width / 2},${height + margin.top + 20})`)
+    .style("text-anchor", "middle")
+    .text("Boroughs");
+
+    svgInjured.append("text")
+    .attr("x", width / 2)
+    .attr("y", 0 - (margin.top / 2))
+    .attr("text-anchor", "middle")
+    .style("font-size", "18px")
+    .text("People Injured in 2021 and 2022");
+
+  // Add Y-axis label
+  svgInjured.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Number of People Injured");
+
+  svgInjured.append("g")
+    .call(d3.axisLeft(yScaleInjured));
+}
 
 
